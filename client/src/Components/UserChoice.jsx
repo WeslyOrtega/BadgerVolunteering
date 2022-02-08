73bc40ab99f9f33218c7e8badaf3a96bf4039e33
Howl1935 <travisphawley@gmail.com>
2022-02-07 19:44:51 -0800

    update

--- client/src/Components/UserChoice.jsx
+++ client/src/Components/UserChoice.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-function UserChoice() {
+function UserChoice({ data }) {
+  const { img, text } = data;
   return (
     <>
-      <h1>USER CHOICE</h1>
+      <img src={img} alt="image of stuff"></img>
+      <div className="flex items-center justify-center p-2 bg-red-100 mt-2 rounded-md">
+        <span className="text-4xl">{text}</span>
+      </div>
     </>
   );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,49 @@
-import React from "react";
+import { useState } from "react";
 import UserChoice from "../components/UserChoice";
+import img1 from "../assets/pexels.jpg";
+import img2 from "../assets/pexels2.jpg";
 
 function Home() {
+  const [data] = useState({
+    _id: "1",
+    data1: {
+      _id: "snoopy",
+      text: "Date me",
+      img: img1,
+    },
+    data2: {
+      _id: "poopy",
+      text: "hate me",
+      img: img2,
+    },
+  });
+
+  const [resp, setResp] = useState({
+    _id: "",
+    choiceId: "",
+    token: null,
+  });
+
+  const onClick = () => {};
+
+  const { data1, data2 } = data;
   return (
     <div className="pt-10 pb-10">
-      <div className="grid grid-cols-2 gap-4 ">
-        <div className="bg-red-100 rounded-xl">
-          <UserChoice />
+      <div className="grid grid-cols-2 gap-5 ">
+        <div className="bg-red-100 rounded-lg">
+          <div className="bg-red-200 m-2 p-2 translate-y-2 translate-x-2 rounded-lg">
+            <UserChoice data={data1} onClick={onClick} />
+          </div>
         </div>
         <div className="bg-red-100 rounded-xl">
-          <UserChoice />
+          <div className="bg-red-200 m-2 p-2 translate-y-2 translate-x-2 rounded-lg">
+            <UserChoice data={data2} onClick={onClick} />
+          </div>
         </div>
       </div>
-      <div className="bg-red-100 rounded-xl p-5 mt-5">Undecided</div>
+      <div className="bg-red-100 rounded-xl p-5 mt-5">
+        <button onClick={onClick}>UNDECIDED</button>
+      </div>
     </div>
   );
 }
