ff98cf4b4f3d8c34669669d4f222aea4b2fd651b
Wesly Ortega <weslyortega@gmail.com>
2022-02-28 18:01:05 -0800

    New 'begin' route

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask.helpers import send_from_directory
 from flask_cors import CORS, cross_origin
 from data_model import Model
+import uuid
 
 app = Flask(__name__, static_folder='client/build', static_url_path='')
 CORS(app)
@@ -29,6 +30,25 @@ def choice():
     response["choices"] = choices
 
     return response
+
+
+@app.route('/api/begin', methods=['GET'])
+@cross_origin()
+def begin():
+
+    user_id = uuid.uuid4()
+
+    return {
+        "user_token": str(user_id),
+        "_id": "6217035701d269f6df9092f0",
+        "name": "Work with People/Not work with people",
+        "option1_destination": "6216ff0201d269f6df9092ed",
+        "option1_obj": "621706bc01d269f6df9092f5",
+        "option2_destination": "6217000401d269f6df9092ee",
+        "option2_obj": "6217074f01d269f6df9092f8",
+        "isFinal": False,
+        "final": []
+    }
 
 
 @app.route('/')
