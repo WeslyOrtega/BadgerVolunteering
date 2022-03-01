da2a8f630aa322c566edf91fc7226cd53b462329
Wesly Ortega <weslyortega@gmail.com>
2022-02-28 18:44:06 -0800

    new 'begin' route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask.helpers import send_from_directory
 from flask_cors import CORS, cross_origin
-from data_model import Model
+from data_model import Nodes_DB, Options_DB
 import uuid
 
 app = Flask(__name__, static_folder='client/build', static_url_path='')
@@ -23,8 +23,7 @@ def choice():
     response = {"id": "someID"}
 
     choices = [
-        Model.find_by_id(Model(), "6204877c11377c28cac4cafa"),
-        Model.find_by_id(Model(), "6204889811377c28cac4cafb")
+        
     ]
 
     response["choices"] = choices
@@ -36,19 +35,21 @@ def choice():
 @cross_origin()
 def begin():
 
-    user_id = uuid.uuid4()
+    start_node: dict = Nodes_DB().find_by_id("6217035701d269f6df9092f0")
 
-    return {
-        "user_token": str(user_id),
-        "_id": "6217035701d269f6df9092f0",
-        "name": "Work with People/Not work with people",
-        "option1_destination": "6216ff0201d269f6df9092ed",
-        "option1_obj": "621706bc01d269f6df9092f5",
-        "option2_destination": "6217000401d269f6df9092ee",
-        "option2_obj": "6217074f01d269f6df9092f8",
-        "isFinal": False,
-        "final": []
-    }
+    res = start_node.copy()
+
+    obj1 = Options_DB().find_by_id(res["option1_obj"])
+    obj2 = Options_DB().find_by_id(res["option2_obj"])
+
+    res["option1_obj"] = obj1
+    res["option2_obj"] = obj2
+
+    res["user_token"] = str(uuid.uuid4())
+
+    print(res)
+
+    return res
 
 
 @app.route('/')
