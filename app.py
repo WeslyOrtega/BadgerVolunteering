eaa469088eee8f6d3ae3f3e6f51fee6799b01599
Wesly Ortega <weslyortega@gmail.com>
2022-04-24 13:57:53 -0700

    updated to satisfy complexity thresholds

--- app.py
+++ app.py
@@ -35,118 +35,121 @@ def review():
     return {}, 204
 
 
+def get_node():
+    token = request.headers.get("user_token")
+    if not token:
+        return {"err": "Missing user token"}, 400
+
+    node_id = request.args.get("node_id")
+    if not node_id:
+        return {"err": "No node id was provided"}, 400
+
+    node = Nodes_DB().find_by_id(node_id)
+    if not node:
+        return {"err": "Node not found"}, 404
+
+    logger.log_user_choice(token, node_id, node['isFinal'])
+
+    if node['isFinal']:
+        return {"final": node["final"], "isFinal": True}, 200
+
+    res = node.copy()
+
+    del(res['_id'])
+
+    if res["option1_obj"]:
+        obj1 = Options_DB().find_by_id(res["option1_obj"])
+        if obj1:
+            del(obj1["_id"])
+        res["option1_obj"] = obj1
+
+    if res["option2_obj"]:
+        obj2 = Options_DB().find_by_id(res["option2_obj"])
+        if obj2:
+            del(obj2["_id"])
+        res["option2_obj"] = obj2
+
+    return res, 200
+
+
+def create_node():
+    req = request.get_json(force=True)
+
+    new_node = {
+        "img": req.get("img", ""),
+        "name": req.get("name", ""),
+        "option1_destination": req.get("option1_destination", ""),
+        "option2_destination": req.get("option2_destination", ""),
+        "option1_obj": req.get("option1_obj", ""),
+        "option2_obj": req.get("option2_obj", ""),
+        "isFinal": req.get("isFinal", False)
+    }
+
+    return Nodes_DB(new_node).save(), 200
+
+
+def update_node():
+    # Check if environment is in dev mode
+    if os.environ['FLASK_ENV'] != 'development':
+        return {"err": "operation not allowed"}, 400
+
+    # Check if environment is in dev mode
+    if os.environ['FLASK_ENV'] != 'development':
+        return {"err": "operation not allowed"}, 400
+
+    node_id = request.args.get("node_id")
+    if not node_id:
+        return {"err": "No node id was provided"}, 400
+
+    node = Nodes_DB().find_by_id(node_id)
+    if not node:
+        return {"err": "Node not found"}, 404
+
+    req = request.get_json(force=True)
+
+    for key in node.keys():
+        content = req.get(key, None)
+        if content is not None and key != "_id":
+            node[key] = content
+
+    Nodes_DB(node).save()
+
+    return node, 200
+
+
+def delete_node():
+    # Check if environment is in dev mode
+    if os.environ['FLASK_ENV'] != 'development':
+        return {"err": "operation not allowed"}, 400
+
+    node_id = request.args.get("node_id")
+    if not node_id:
+        return {"err": "No node id was provided"}, 400
+
+    node = Nodes_DB().find_by_id(node_id)
+    if not node:
+        return {"err": "Node not found"}, 404
+
+    Nodes_DB(node).remove()
+
+    return {}, 204
+
+
 @app.route('/api/node', methods=['GET', 'POST', 'PATCH', 'DELETE'])
 @cross_origin()
 def choice():
 
-    # Start GET method
     if request.method == "GET":
+        return get_node()
 
-        token = request.headers.get("user_token")
-        if not token:
-            return {"err": "Missing user token"}, 400
-
-        node_id = request.args.get("node_id")
-        if not node_id:
-            return {"err": "No node id was provided"}, 400
-
-        node = Nodes_DB().find_by_id(node_id)
-        if not node:
-            return {"err": "Node not found"}, 404
-
-        logger.log_user_choice(token, node_id, node['isFinal'])
-
-        if node['isFinal']:
-            return {"final": node["final"], "isFinal": True}, 200
-
-        res = node.copy()
-
-        del(res['_id'])
-
-        if res["option1_obj"]:
-            obj1 = Options_DB().find_by_id(res["option1_obj"])
-            if obj1:
-                del(obj1["_id"])
-            res["option1_obj"] = obj1
-
-        if res["option2_obj"]:
-            obj2 = Options_DB().find_by_id(res["option2_obj"])
-            if obj2:
-                del(obj2["_id"])
-            res["option2_obj"] = obj2
-
-        return res, 200
-    # End GET method
-
-    # Start POST method
     elif request.method == 'POST':
+        return create_node()
 
-        # Check if environment is in dev mode
-        if os.environ['FLASK_ENV'] != 'development':
-            return {"err": "operation not allowed"}, 400
-
-        req = request.get_json(force=True, silent=True)
-
-        new_node = {
-            "img": req.get("img", ""),
-            "name": req.get("name", ""),
-            "option1_destination": req.get("option1_destination", ""),
-            "option2_destination": req.get("option2_destination", ""),
-            "option1_obj": req.get("option1_obj", ""),
-            "option2_obj": req.get("option2_obj", ""),
-            "isFinal": req.get("isFinal", False)
-        }
-
-        return Nodes_DB(new_node).save(), 200
-
-    # End POST method
-
-    # Start PUT method
     elif request.method == 'PATCH':
+        return update_node()
 
-        # Check if environment is in dev mode
-        if os.environ['FLASK_ENV'] != 'development':
-            return {"err": "operation not allowed"}, 400
-
-        node_id = request.args.get("node_id")
-        if not node_id:
-            return {"err": "No node id was provided"}, 400
-
-        node = Nodes_DB().find_by_id(node_id)
-        if not node:
-            return {"err": "Node not found"}, 404
-
-        req = request.get_json()
-
-        for key in node.keys():
-            content = req.get(key, None)
-            if content is not None and key is not "_id":
-                node[key] = content
-
-        Nodes_DB(node).save()
-
-        return node, 200
-    # End POST method
-
-    # Start DELETE method
     elif request.method == 'DELETE':
-
-        # Check if environment is in dev mode
-        if os.environ['FLASK_ENV'] != 'development':
-            return {"err": "operation not allowed"}, 400
-
-        node_id = request.args.get("node_id")
-        if not node_id:
-            return {"err": "No node id was provided"}, 400
-
-        node = Nodes_DB().find_by_id(node_id)
-        if not node:
-            return {"err": "Node not found"}, 404
-
-        Nodes_DB(node).remove()
-
-        return {}, 204
-    # End DELETE method
+        return delete_node()
 
 
 @app.route('/api/begin', methods=['GET'])
