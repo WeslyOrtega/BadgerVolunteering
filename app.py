2bdef2c0f6d077755195667fc61b77f1b24f4938
Wesly Ortega <weslyortega@gmail.com>
2022-04-24 13:04:29 -0700

    Added methods skeleton to /node route

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from flask_cors import CORS, cross_origin
 from data_model import Nodes_DB, Options_DB
 from logger import Logger
 
+import os
+
 app = Flask(__name__, static_folder='client/build', static_url_path='')
 CORS(app)
 
@@ -33,44 +35,75 @@ def review():
     return {}, 204
 
 
-@app.route('/api/node', methods=['GET'])
+@app.route('/api/node', methods=['GET', 'POST', 'PUT', 'DELETE'])
 @cross_origin()
 def choice():
 
-    token = request.headers.get("user_token")
-    if not token:
-        return {"err": "Missing user token"}, 400
+    # Start GET method
+    if request.method == "GET":
 
-    node_id = request.args.get("node_id")
-    if not node_id:
-        return {"err": "No node id was provided"}, 400
+        token = request.headers.get("user_token")
+        if not token:
+            return {"err": "Missing user token"}, 400
 
-    node = Nodes_DB().find_by_id(node_id)
-    if not node:
-        return {"err": "Node not found"}, 404
+        node_id = request.args.get("node_id")
+        if not node_id:
+            return {"err": "No node id was provided"}, 400
 
-    logger.log_user_choice(token, node_id, node['isFinal'])
+        node = Nodes_DB().find_by_id(node_id)
+        if not node:
+            return {"err": "Node not found"}, 404
 
-    if node['isFinal']:
-        return {"final": node["final"], "isFinal": True}, 200
+        logger.log_user_choice(token, node_id, node['isFinal'])
 
-    res = node.copy()
+        if node['isFinal']:
+            return {"final": node["final"], "isFinal": True}, 200
 
-    del(res['_id'])
+        res = node.copy()
 
-    obj1 = Options_DB().find_by_id(res["option1_obj"])
-    obj2 = Options_DB().find_by_id(res["option2_obj"])
+        del(res['_id'])
 
-    if obj1:
-        del(obj1["_id"])
+        if res["option1_obj"]:
+            obj1 = Options_DB().find_by_id(res["option1_obj"])
+            if obj1:
+                del(obj1["_id"])
+            res["option1_obj"] = obj1
 
-    if obj2:
-        del(obj2["_id"])
+        if res["option2_obj"]:
+            obj2 = Options_DB().find_by_id(res["option2_obj"])
+            if obj2:
+                del(obj2["_id"])
+            res["option2_obj"] = obj2
 
-    res["option1_obj"] = obj1
-    res["option2_obj"] = obj2
+        return res, 200
+    # End GET method
 
-    return res, 200
+    # Start POST method
+    elif request.method == 'POST':
+
+        # Check if environment is in dev mode
+        if os.environ['FLASK_ENV'] != 'development':
+            return {"err": "operation not allowed"}, 400
+        pass
+    # End POST method
+
+    # Start PUT method
+    elif request.method == 'PUT':
+
+        # Check if environment is in dev mode
+        if os.environ['FLASK_ENV'] != 'development':
+            return {"err": "operation not allowed"}, 400
+        pass
+    # End POST method
+
+    # Start DELETE method
+    elif request.method == 'DELETE':
+
+        # Check if environment is in dev mode
+        if os.environ['FLASK_ENV'] != 'development':
+            return {"err": "operation not allowed"}, 400
+        pass
+    # End DELETE method
 
 
 @app.route('/api/begin', methods=['GET'])
